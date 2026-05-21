bf785e609ae0c72f9b7e4d751c21aa1963b37615
unknown <harshverma0014@gmail.com>
2026-05-21 19:22:20 +0530

    Update

--- Python/after merge.py
+++ Python/after merge.py
@@ -33,15 +33,15 @@
 
 
 
-import pymysql as mysql
-try:
-    db = mysql.connect(host='localhost',port=3306,user='root',password='1234',database='my_industry')
-    smt=db.cursor()
-    s="CREATE TABLE products(ProductID INT PRIMARY KEY,ProductName VARCHAR(100),SupplierID INT,CategoryID INT,QuantityPerUnit INT,UnitPrice DECIMAL(10,2),UnitsInStock INT,UnitsOnOrder INT,ReorderLevel INT,Discontinued VARCHAR(20),ManufactureDate DATE)";
-    smt.execute(s)
-    print("table created successfully...")
-except Exception as e:
-   print("error:",e)
+# import pymysql as mysql
+# try:
+#     db = mysql.connect(host='localhost',port=3306,user='root',password='1234',database='my_industry')
+#     smt=db.cursor()
+#     s="CREATE TABLE products(ProductID INT PRIMARY KEY,ProductName VARCHAR(100),SupplierID INT,CategoryID INT,QuantityPerUnit INT,UnitPrice DECIMAL(10,2),UnitsInStock INT,UnitsOnOrder INT,ReorderLevel INT,Discontinued VARCHAR(20),ManufactureDate DATE)";
+#     smt.execute(s)
+#     print("table created successfully...")
+# except Exception as e:
+#    print("error:",e)
 
 
 
@@ -50,29 +50,72 @@ except Exception as e:
 
 
 
-import pymysql as mysql
-try:
-    db = mysql.connect(host='localhost',port=3306,user='root',password='1234',database='my_industry')
-    smt=db.cursor()
-    pid=input("enter productid:")
-    pn=input("enter productname:")
-    sid=input("enter supplierid:")
-    cid=input("enter categoryid:")
-    qperunit=input("enter quantityperunit:")
-    uprice=input("enter umitprice:")
-    us=input("enter unitsinstock:")
-    uo=input("enter unitsonorder:")
-    rl=input("enter reorderlevel:")
-    d=input("enter discountinued:")
-    mf=input("Enter Manufacture date:")
-    q=f"(insert into products values({pid},'{pn}',{sid},{cid},{qperunit},{uprice},{us},{uo},{rl},'{d}','{mf}')"
-    print(q)
-    smt.execute(q)
-    print("products submitted successfully...")
-    db.commit()
-    db.close()
-except Exception as e:
-    print("error:",e)
+# import pymysql as mysql
+# try:
+#     db = mysql.connect(host='localhost',port=3306,user='root',password='1234',database='my_industry')
+#     smt=db.cursor()
+#     pid=input("enter productid:")
+#     pn=input("enter productname:")
+#     sid=input("enter supplierid:")
+#     cid=input("enter categoryid:")
+#     qperunit=input("enter quantityperunit:")
+#     uprice=input("enter umitprice:")
+#     us=input("enter unitsinstock:")
+#     uo=input("enter unitsonorder:")
+#     rl=input("enter reorderlevel:")
+#     d=input("enter discountinued:")
+#     mf=input("Enter Manufacture date:")
+#     q=f"(insert into products values({pid},'{pn}',{sid},{cid},{qperunit},{uprice},{us},{uo},{rl},'{d}','{mf}')"
+#     print(q)
+#     smt.execute(q)
+#     print("products submitted successfully...")
+#     db.commit()
+#     db.close()
+# except Exception as e:
+#     print("error:",e)
+
+
+
+
+# ----------------------------------------------------------
+
+
+# l=39,40 
+
+
+
+# -------------------------------------------------------------
+
+
+
+# ----------------------------------------------------------
+
+
+# l=41 april 21
+
+
+
+# -------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
