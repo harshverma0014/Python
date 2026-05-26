6f0f34a425bb4b5e10f681de9a24ddadac4383d0
unknown <harshverma0014@gmail.com>
2026-05-26 19:01:12 +0530

    update

---
+++ Python/streamlit.py
@@ -0,0 +1,100 @@
+import streamlit as st
+
+
+
+# st.title("TITLE")
+# st.header("header")
+# st.subheader("subheader")
+# st.text("text")
+# st.write("write")
+# st.error("error")
+# st.warning("warning")
+# st.info("info")
+# st.success("success")
+
+
+
+
+
+# st.markdown("""
+# # big
+# ## small
+# ### litle
+# """)
+
+# #  big
+# ## small
+# ### small
+
+
+
+# st.markdown("<h1 style='color: green;'>Hello, World!</h1>", unsafe_allow_html=True)
+
+
+# student=[{"roll":100,"name":"peter singh"}]
+# st.dataframe(student)
+# st.table(student)
+
+
+
+# id=st.text_input("enter id")
+
+
+# label="" ,placeholder="enter id"
+
+# value="", 
+
+# max_chars= none ,
+
+# key=none,
+
+# type="default/passwaord"
+
+# help = nane,
+
+# autocomplete = none/ email / name / password ,
+
+# on_change = none ,
+
+# args = none,
+
+# kwatgs= none,
+
+# disabled = false,
+# label_visibility="visible/hidden"
+
+
+
+
+
+
+
+
+
+
+
+
+# a=st.number_inpu("enterr no.)
+
+# min_value=none,
+# max_value=none,
+# value="min",
+# step=none
+# format=none,
+# key=none,
+# help=none,
+# on_chamge=none,
+# args=none,
+# kwargs=none,
+# placeholder=none,
+# disabled=false,
+# label_visibility="visible"
+
+
+# java=st.checkbox("java")
+
+
+# g=t.radio("gender"=,["male","female","others"])
+
+# at.selectbox()
+
