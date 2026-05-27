bba65bb50ae1a72db6c00073271e28a738ae4c03
unknown <harshverma0014@gmail.com>
2026-05-27 17:10:43 +0530

    update

--- Python/streamlit.py
+++ Python/streamlit.py
@@ -118,45 +118,178 @@ import streamlit as st
 
 
 
-import streamlit as st
+# import streamlit as st
 
-import pymysql as sql
-
-
-
-st.title("employee management system")
+# import pymysql as sql
 
 
 
-eid=st.text_input("enter employee id")
-en=st.text_input("enter employee name")
-eg=st.selectbox("gender",["male","female","others"])
-dob=st.text_input("enter employee date of birth")
-ec=st.selectbox("enter employee city",["delhi","mumbai","kolkata","chennai"])
-es=st.text_input("enter employee salary")
-em=st.text_input("enter employee no.")
 
 
-if st.button("submit"):
- try:
-    db = sql.connect(
-    host ='localhost',
-    port=3306,
-    user='root',
-    password='1234',
-    database='testyo' ,
-    cursorclass=sql.cursors.DictCursor
-    )
-    smt = db.cursor()
+# data entery
 
-    s=f"insert into new_table values ( {eid} , '{en}', '{dob}' , '{eg}' , '{ec}' , '{es}' , '{em}' ) "
-    smt.execute(s)
-    # print(s)
-    db.commit()
-    st.success("data inserted successfully")
- except Exception as e:
-    st.error(e)
- finally:
+# st.title("employee management system")
 
-    db.close()
+
+
+# eid=st.text_input("enter employee id")
+# en=st.text_input("enter employee name")
+# eg=st.selectbox("gender",["male","female","others"])
+# dob=st.text_input("enter employee date of birth")
+# ec=st.selectbox("enter employee city",["delhi","mumbai","kolkata","chennai"])
+# es=st.text_input("enter employee salary")
+# em=st.text_input("enter employee no.")
+
+
+# if st.button("submit"):
+#  try:
+#     db = sql.connect(
+#     host ='localhost',
+#     port=3306,
+#     user='root',
+#     password='1234',
+#     database='testyo' ,
+#     cursorclass=sql.cursors.DictCursor
+#     )
+#     smt = db.cursor()
+
+#     s=f"insert into new_table values ( {eid} , '{en}', '{dob}' , '{eg}' , '{ec}' , '{es}' , '{em}' ) "
+#     smt.execute(s)
+#     # print(s)
+#     db.commit()
+#     st.success("data inserted successfully")
+#  except Exception as e:
+#     st.error(e)
+#  finally:
+
+#     db.close()
+     
+
+
+
+# displayall
+
+# try:
+#     db = sql.connect(
+#     host ='localhost',
+#     port=3306,
+#     user='root',
+#     password='1234',
+#     database='power_bi' ,
+#     cursorclass=sql.cursors.DictCursor
+#     )
+#     smt=db.cursor()
+#     smt.execute("select * from employees")
+#     records=smt.fetchall()
+#     st.dataframe(records)
+# except Exception as e:
+#     st.error(e)
+# finally:
+#     db.close()
+
+
+
+
+
+
+
+
+
+# EDIT
+
+
+# import streamlit as st
+# try:
+#     import pymysql as mysql
+#     db = mysql.connect(host='localhost',port=3306,user='root',password='1234',database='sample',cursorclass=mysql.cursors.DictCursor)
+
+#     smt=db.cursor()
+#     id=st.text_input("Enter Employee id : ")
+#     smt.execute(f'Select * from employee where employeeid={id}')
     
+#     record=smt.fetchone()
+#     if (record):
+#         st.write("Employee id :",record['EmployeeID'])
+#         st.write("1] Employee Name :",record['EmployeeName'])
+#         st.write("2] Employee city :",record['City'])
+#         st.write("3] Employee salary :",record['Salary'])
+#         st.write("4] Exit")
+#         ch=st.number_input('Enter Choice[1-4] :', min_value=1, max_value=4)
+#         pat=''
+#         if (ch==1):
+#             en=st.text_input("Enter New Employee Name : ")
+#             pat=f'employeename="{en}"'
+#         elif (ch==2):
+#             ec=st.text_input("Enter New Employee City : ")
+#             pat=f'city="{ec}"'
+#         elif (ch==3):
+#             es=st.number_input("Enter New Employee Salary : ")
+#             pat=f'salary="{es}"'
+#         elif (ch==4):
+#             pat=''
+#         else:
+#             st.error("Wrong option..")
+        
+#         if (st.button("update")):
+#          if(pat!=''):
+#           q=f"update employee set {pat} where employeeid={id}"
+#           smt.execute(q)
+#           db.commit()
+#           st.success('Employee update successfully....')
+#          else:
+#             st.success("Exit successfully....")
+    
+ 
+
+# except Exception as e:
+#     st.error(e)
+# finally:
+#     db.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# delete
+
+# import streamlit as st
+# try:
+#     import pymysql as mysql
+#     db = mysql.connect(host='localhost',port=3306,user='root',password='1234',database='sample',cursorclass=mysql.cursors.DictCursor)
+
+#     smt=db.cursor()
+#     id=st.text_input("Enter Employee ID u want to delete :  ")
+#     smt.execute(f'Select * from employee where employeeid={id}')
+    
+#     record=smt.fetchone()
+#     if (record):
+#         st.write("Employee id :",record['EmployeeID'])
+#         st.write("Employee Name :",record['EmployeeName'])
+#         st.write("Employee city :",record['City'])
+#         st.write("Employee salary :",record['Salary'])
+        
+#         ch=st.text_input('Do you want to delete above record y/n:')
+#         if(ch.lower()=='y'):
+#          q=f"delete from employee where employeeid={id}"
+#          smt.execute(q)
+#          db.commit()
+#          st.success('Employee deleted successfully....')
+#         elif(ch.lower()=='n'):
+#             st.info('Deletion cancelled.')
+#     else:
+#         st.error(f'Employee not exist {id}')
+    
+ 
+
+# except Exception as e:
+#     st.error(e)
+# finally:
+#     db.close()
